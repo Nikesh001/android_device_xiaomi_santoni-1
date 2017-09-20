c46adcc89ec3ce13546fa7bcf2ee023163106f6e
Swapnil <25403317+SwapnilSoni1999@users.noreply.github.com>
2017-09-20 19:22:58 +0530

    build oreo HALs
    added under tinymix

--- device.mk
+++ device.mk
@@ -82,6 +82,14 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     libqcompostprocbundle \
     tinymix
+    
+
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl
+
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl
